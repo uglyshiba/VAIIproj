12d32ad18235e20c1c9baa9ad15484916512f53c
uglyshiba <uglyshiba@proton.me>
2023-10-25 14:50:19 +0200

    Added letter color randomization Javascript

--- js/randomizeLetterColor.js
+++ js/randomizeLetterColor.js
@@ -1,12 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    randomizeLetterColor("nadpis");
+});
+
 function randomizeLetterColor(elementId) {
-    var element = document.getElementById(elementId)
+    var element = document.getElementById(elementId);
+    if (element) {
+        var text = element.textContent;
+        element.style.color = getRandomColor(); // Set initial text color
+        var changedText = text.split('').map(function(letter) {
+            var randomColor = getRandomColor();
+            return '<span style="color:' + randomColor + ';">' + letter + '</span>';
+        }).join('');
+        element.innerHTML = changedText;
+    }
 }
 
 function getRandomColor() {
-    var randRed = Math.floor(Math.random() * 200);
-    var randGreen = Math.floor(Math.random() * 200);
-    var randBlue = Math.floor(Math.random() * 200);
+    var randRed = Math.floor(Math.random() * 220 + 20);
+    var randGreen = Math.floor(Math.random() * 220 + 20);
+    var randBlue = Math.floor(Math.random() * 200 + 20);
 
     var color = 'rgb(' + randRed + ',' + randGreen + ',' + randBlue + ')';
-    return color
+    return color;
 }
